2c77aaf1f289877f0cd3cd89a42a2ee97da48e74
Claudia Lizeth Zamora Barahona <claudializeth_07@hotmail.com>
2021-11-27 16:57:13 -0600

    Guardar Cambios

---
+++ CatraSports-main/CatraSports/CatraSports.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CatraSports.BL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatraSports.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminwebsiteUrl =
+                ConfigurationManager.AppSettings["adminwebsiteUrl"];
+
+            return View(listadeProductos);
+        }
+    }
+}
---
+++ CatraSports-main/CatraSports/CatraSports.Win/Form2.cs
@@ -0,0 +1,38 @@
+﻿using CatraSports.BL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatraSports.Win
+{
+    public partial class Form2 : Form
+    {
+        ReporteVentasPorProductoBL _reporteVentasPorProductoBL;
+
+        public Form2()
+        {
+            InitializeComponent();
+            _reporteVentasPorProductoBL = new ReporteVentasPorProductoBL();
+
+            RefrescarDatos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RefrescarDatos();
+        }
+
+        private void RefrescarDatos()
+        {
+            var listaVentasPorProducto = _reporteVentasPorProductoBL.ObtenerVentasPorProducto();
+            listaVentasPorProductoBindingSource.DataSource = listaVentasPorProducto;
+            listaVentasPorProductoBindingSource.ResetBindings(false);
+        }
+    }
+}
